a2141ad780f584dc4e258722da2e256340e5ce06
Ryu <zsplitt014@gmail.com>
2025-09-12 10:17:14 +0200

    Add new styles

--- src/app/controllers/register.php
+++ src/app/controllers/register.php
@@ -38,6 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
     <button class="theme-toggle" id="themeToggle" aria-label="Toggle theme"></button>
     <img src="../../public/images/logo.png" alt="Logo" class="logo-website-top-left">
+    <div class="logo-shadow"></div>
     <div class="container page">
         <div class="card">
             <h1>Register</h1>
