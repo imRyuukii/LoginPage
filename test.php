e987d1c41e526ab1972ba8b27025599d1d789b66
Ignacy Jan Gospodarczyk <zsplitt014@gmail.com>
2025-10-18 00:39:45 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
 try {
-    $db = new PDO('mysql:host=127.0.0.1;dbname=login_system;charset=utf8mb4', 'root', '');
+    $db = new PDO(
+        "mysql:host=127.0.0.1;dbname=login_system;charset=utf8mb4",
+        "rootphp",
+        "",
+    );
     echo "Connected!";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
